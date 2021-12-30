a244473f261aa7c804fac6d6a1312ae4f1544a07
Xia Tershi <39051261+mmm25002500@users.noreply.github.com>
2021-12-30 21:20:45 +0800

    Add Feature: Time Remain of this year

--- src/ctbot/command/slash/time.py
+++ src/ctbot/command/slash/time.py
@@ -7,9 +7,9 @@ from discord_slash.model import SlashCommandOptionType
 from ..utils import cog_slash_managed, gen_list_of_option_choices
 
 # NOTE: something need to change cause it's wrong
-when_cap_111  = '2022/06/04 08:30' # 111 會考日期
+when_cap_111  = '2022/05/14 08:30' # 111 會考日期
 when_tcte_111 = '2022/05/07 10:15' # 111 統測日期
-when_ceec_111 = '2022/05/15 09:20' # 111 學測日期
+when_ceec_111 = '2022/01/15 09:20' # 111 學測日期
 # ceecCountDown111 = datetime(2022,1,15,9,20) # 111學測日期
 when_tershi_18 = '2022/05/26' # 夏特稀 111 生日
 yahoo_knowledge_discontinued = '2021/05/04' # 奇摩知識家停止服務
@@ -33,13 +33,22 @@ def get_exam_days_left():
     text += '=====111年=====\n'
     text += f'{when_tershi_18} 夏特稀皇帝18歲誕辰倒數 {get_days_left(when_tershi_18, date_format)} \n'
     text += f'{when_cap_111 } 會考倒數：\t{get_days_left(when_cap_111, datetime_format)} \n'
-    text += f'{when_tcte_111} 學測倒數：\t{get_days_left(when_tcte_111, datetime_format)} \n'
-    text += f'{when_ceec_111} 統測倒數：\t{get_days_left(when_ceec_111, datetime_format)} \n'
+    text += f'{when_ceec_111} 學測倒數：\t{get_days_left(when_tcte_111, datetime_format)} \n'
+    text += f'{when_tcte_111} 統測倒數：\t{get_days_left(when_ceec_111, datetime_format)} \n'
     text += '\n各位中華帝國的子民的，有什麼需要倒數的，或是日程，可以與 @TershiXia聯絡喔！\n'
-    text += '111會考生: Cute USB#5387 \n'
-    text += '111學測升: @拉拉拉拉 \n'
-    text += '111統測生: Cute USB#5387 \n'
+    text += '111會考生: Cute USB#5387 , 嘎逼#1596 , 祥翔#4073\n'
+    text += '111學測生: @拉拉拉拉 \n'
+    text += '111統測生: 夏特稀#3716 \n'
     return text
+
+def get_remain_time(year, month, day, hour, minute, second):
+    d1 = datetime(year,1,1)
+    d2 = datetime(year,month,day)
+    remain_day = d2 - d1
+    remain_time = ((remain_day.days *24 + hour)*60+minute)*60+second
+    remain_time = remain_time / 31536000
+    remain_time = "%.2f%%" % (remain_time * 100)
+    return remain_time
 
 class SlashTime(commands.Cog):
     def __init__(self, client: discord.Client):
@@ -60,6 +69,14 @@ class SlashTime(commands.Cog):
     @cog_slash_managed(base='time', description='特別日倒數計時')
     async def special_days_left(self, ctx):
         await ctx.send(get_exam_days_left())
+    
+    @cog_slash_managed(base='time', description='今年已經過了多少百分比')
+    async def remain_time_left(self, ctx):
+        today = datetime.now()
+        year, month, day, hour, minute, second, week = today.timetuple()[:7]
+        remain_time = get_remain_time(year, month, day, hour, minute, second)
+        text = str(year) + '年已經過了' + str(remain_time)
+        await ctx.send(text)
 
     @cog_slash_managed(base='time', description='現在時間',
         options=[create_option('format', '格式', 
