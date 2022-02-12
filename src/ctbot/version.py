7e25132f4b6f8875e91c357fd6500621b7cb03f4
TershiXia <39051261+mmm25002500@users.noreply.github.com>
2022-02-12 17:45:05 +0800

    Update to v2.8

--- src/ctbot/version.py
+++ src/ctbot/version.py
@@ -1,27 +1,51 @@
 author = {
-	'icon': 'https://tershi.cutespirit.org/Cutespirit/icon.png',
-	'age': '17',
+    'name' : '夏特稀',
+    'gender' : '男',
+	'description': '一個17歲的台灣人，學習程式語言',
+    'icon': 'https://tershi.cutespirit.org/images/tershi.jpg',
+    'url' : 'https://tershi.cutespirit.org',
+    'age': '17',
 	'grade': '高三',
 	'use_os': 'Arch-Linux',
-	'description': '我們是一群在網路上熱愛寫程式的學生',
-	'experience': '想給每一個discord熱愛者而做的bot'
+    'birthday' : '中華民國111年05月26日'
 }
 
 bot = {
-	'icon': 'https://tershi.cutespirit.org/Cutespirit/icon.png',
+    'name' : '靈萌Bot',
+    'url' : 'https://github.com/Cutespirit-Team/CutespiritDiscordBot',
+    'icon': 'https://tershi.cutespirit.org/Cutespirit/icon.png',
+	'description': '一個由靈萌團隊製作的 Discord 機器人',
+	'experience': '想給每一個discord熱愛者而做的bot',
 	'language': '正體中文',
-	'version': '2.7.1' ,
+	'version': '2.8' ,
 	'develope_date': '2021/07/19',
-	'last_udpate': '2022/01/25'
+	'last_udpate': '2022/01/26'
 }
 
-store = {
-    'store_id': '',
-    'store_key': '',
-    'IV': '',
+team = {
+    'name' : '靈萌團隊',
+    'name-eng' : 'Cutespirit Team',
+    'url' : 'https://www.cutespirit.org',
+    'icon': 'https://tershi.cutespirit.org/Cutespirit/icon.png',  
+    'description': '我們是一群在網路上熱愛寫程式的學生',
+    'founded_day' : '2021/07/15',
+    'teammate_count' : '還沒算',
+    'team_leader' : '夏特稀',
+    'team_coleader' : '無',
+    'team_cofounder' : '夏特稀、CuteUSB、哈密瓜、AstinBob、萌弟(CuteUSB，已退)'
+}
 
+shop = {
+    'name' : 'Cutespirit-SHOP',
+    'description' : '一個低價有誠信的代購商',
+    'url' : 'https://shop.cutespirit.org',
+    'icon': 'https://tershi.cutespirit.org/Cutespirit/icon.png',  
+    'shop_id' : '123',
+    'shop_key' : '123',
+    'IV' : '123'
 }
 
 weather = {
-    'weather_token' : ''
+    'weather_token' : '123'
 }
+
